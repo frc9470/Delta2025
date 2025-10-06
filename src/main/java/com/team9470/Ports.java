5a24fee4a79d8ed589eaf2d270a5d7c7db02e38a
Tycho Young <tychoyoung@gmail.com>
2025-10-05 22:03:25 -0700

    Remove retired manipulator and climber subsystems

--- src/main/java/com/team9470/Ports.java
+++ src/main/java/com/team9470/Ports.java
@@ -9,25 +9,14 @@ public class Ports {
     public static final CanDeviceId ELEVATOR_MAIN = new CanDeviceId(14, "rio");
     public static final CanDeviceId ELEVATOR_FOLLOWER = new CanDeviceId(15, "rio");
 
-    public static final CanDeviceId ALGAE_PIVOT = new CanDeviceId(18, "rio");
-
-    public static final CanDeviceId CORAL_INTAKE = new CanDeviceId(16, "rio");
-    public static final CanDeviceId FUNNEL = new CanDeviceId(19, "rio");
-
     public static final CanDeviceId CANdle = new CanDeviceId(20, "rio");
-
-    public static final CanDeviceId CLIMBER_MAIN = new CanDeviceId(21, "rio");
-    public static final CanDeviceId CLIMBER_FOLLOWER = new CanDeviceId(22, "rio");
-
-    public static final CanDeviceId CLIMBER_WHEELS = new CanDeviceId(23, "rio");
-    public static final CanDeviceId FUNNEL_CONTROL = new CanDeviceId(24, "rio");
 
     //placeholders:
     public static final CanDeviceId INDEXER_1 = new CanDeviceId(0, "rio");
     public static final CanDeviceId INDEXER_2 = new CanDeviceId(1, "rio");
 
     /**
-     * Beam Break IDs
+     * Sensor IDs
      */
-    public static final int CORAL_BREAK = 1;
+    public static final int INDEXER_SENSOR = 1;
 }
